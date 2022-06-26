a8f52b30b97351b632717a43aa3a88a8d0c367eb
GinSanaduki <48995370+GinSanaduki@users.noreply.github.com>
2022-06-27 00:00:15 +0900

    「家賃支援給付金の不正受給者の認定及び公表について」についての追加対応
    「家賃支援給付金の不正受給者の認定及び公表について」についての追加対応

---
+++ SQLScript/11_INSERT_RENSEIGNEMENTS_SUR_LE_SITE_Liste_de_certification_des_beneficiaires_frauduleux_pour_les_travailleurs_independants_PRESTATIONS_D_AIDE_AU_LOYER.sql
@@ -0,0 +1,34 @@
+-- 11_INSERT_RENSEIGNEMENTS_SUR_LE_SITE_Liste_de_certification_des_beneficiaires_frauduleux_pour_les_travailleurs_independants_PRESTATIONS_D_AIDE_AU_LOYER.sql
+-- SQLScript/11_INSERT_RENSEIGNEMENTS_SUR_LE_SITE_Liste_de_certification_des_beneficiaires_frauduleux_pour_les_travailleurs_independants_PRESTATIONS_D_AIDE_AU_LOYER.sql
+.bail on
+.output stdout
+
+CREATE TABLE IF NOT EXISTS
+RENSEIGNEMENTS_SUR_LE_SITE_PRESTATIONS_D_AIDE_AU_LOYER
+(
+	CODE_D_IDENTIFICATION TEXT CHECK(CODE_D_IDENTIFICATION IN ('Page d_accueil', 'Liste de certification des beneficiaires frauduleux pour les travailleurs independants', 'Liste de certification des beneficiaires frauduleux pour les Entreprises')),
+	DATE_ET_HEURE_D_ACQUISITION TEXT,
+	BALISE_ENTITE_ETAG_HTTP TEXT,
+	CODES_DE_HACHAGE TEXT,
+	PRIMARY KEY(CODE_D_IDENTIFICATION, DATE_ET_HEURE_D_ACQUISITION)
+)
+;
+
+.parameter init
+.parameter set @DATE_ET_HEURE_D_ACQUISITION "':DATE_ET_HEURE_D_ACQUISITION'"
+
+BEGIN TRANSACTION;
+
+INSERT INTO
+	RENSEIGNEMENTS_SUR_LE_SITE_PRESTATIONS_D_AIDE_AU_LOYER
+SELECT
+	'Liste de certification des beneficiaires frauduleux pour les travailleurs independants' AS CODE_D_IDENTIFICATION,
+	@DATE_ET_HEURE_D_ACQUISITION AS DATE_ET_HEURE_D_ACQUISITION,
+	NULL AS BALISE_ENTITE_ETAG_HTTP,
+	NULL AS CODES_DE_HACHAGE
+on conflict ( CODE_D_IDENTIFICATION, DATE_ET_HEURE_D_ACQUISITION)
+do nothing
+;
+
+COMMIT;
+
