a8084d1d0ac66d36939322c3e51400925966c339
GinSanaduki <48995370+GinSanaduki@users.noreply.github.com>
2022-07-02 23:46:53 +0900

    Add files via upload

--- ShellScript/09_Reset.sh
+++ ShellScript/09_Reset.sh
@@ -4,5 +4,10 @@
 
 sqlite3 SQLITE3/RENSEIGNEMENTS_SUR_LE_SITE.sqlite3 < SQLScript/13_RESET.sql
 rm -f TempSQLScripts/*.* > /dev/null 2>&1
+Date=`date '+%Y%m%d'`
+ls -1 "./StoredHTML/" | \
+fgrep $Date | \
+awk 'BEGIN{print ":";}{print "rm -f \047"$0"\047 > /dev/null 2>&1";}' | \
+sh
 exit 0
 
